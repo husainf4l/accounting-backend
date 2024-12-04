d1d6f8d09984123c01be5059509bcb91f5a5a8de
alhussein abdullah <husain.f4l@gmail.com>
2024-12-04 08:48:10 +0300

    as

---
+++ prisma/migrations/20241204054218_general/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "company" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "company_pkey" PRIMARY KEY ("id")
+);
